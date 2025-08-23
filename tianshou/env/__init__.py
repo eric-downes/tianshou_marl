62ae6fb53ecf52dfecbe1ee2121992b122824122
Eric Downes <eric@triskew.com>
2025-08-23 16:28:47 +0200

    dict observation space implemented; tests pass

--- tianshou/env/__init__.py
+++ tianshou/env/__init__.py
@@ -7,6 +7,7 @@ from tianshou.env.gym_wrappers import (
 )
 from tianshou.env.pettingzoo_env import PettingZooEnv
 from tianshou.env.enhanced_pettingzoo_env import EnhancedPettingZooEnv
+from tianshou.env.dict_observation import DictObservationWrapper, DictToTensorPreprocessor
 from tianshou.env.venv_wrappers import VectorEnvNormObs, VectorEnvWrapper
 from tianshou.env.venvs import (
     BaseVectorEnv,
@@ -26,6 +27,8 @@ __all__ = [
     "VectorEnvNormObs",
     "PettingZooEnv",
     "EnhancedPettingZooEnv",
+    "DictObservationWrapper",
+    "DictToTensorPreprocessor",
     "ContinuousToDiscrete",
     "MultiDiscreteToDiscrete",
     "TruncatedAsTerminated",
